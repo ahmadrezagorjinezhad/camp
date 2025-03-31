4b0813b03e796c8e2bfbfe694263e6b98bc82074
ahmadrezagorjinezhad <ahmadrezagn1386@gmail.com>
2025-03-31 21:18:54 +0330

    camp

---
+++ app.js
@@ -0,0 +1,8 @@
+let navbtn = document.querySelector('.navbtn')
+let mobilenavmenu = document.querySelector('.mobilenavmenu')
+navbtn.addEventListener('click')
+function hambr(){
+    // const tmenue = document.querySelector('.mobilenavmenu')
+    // tmenue.classList.toggle('ac')
+    mobilenavmenu.classList.toggle('ac')
+}
